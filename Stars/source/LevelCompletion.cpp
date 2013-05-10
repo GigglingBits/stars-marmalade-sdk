e86144ebca683e6d9b1970c68d7a99b57b0c026f
Oliver Lanz <oliver.lanz@gigglingbits.com>
2013-05-10 08:39:16 +0200

    FIxed the "Next Level" button

--- Stars/source/LevelCompletion.cpp
+++ Stars/source/LevelCompletion.cpp
@@ -11,7 +11,7 @@ LevelCompletion::LevelCompletion(GameFoundation::CompletionInfo& info) :
     m_xButtonStar(eButtonCommandIdNone, s3eKeyFirst),
     m_xButtonQuit(eButtonCommandIdOpenLevelMenu, s3eKeyAbsGameD),
 	m_xButtonRetry(eButtonCommandIdRestartLevel, s3eKeyAbsGameB),
-	m_xButtonNext(eButtonCommandIdOpenLevel, s3eKeyFirst) {
+	m_xButtonNext(eButtonCommandIdOpenNextLevel, s3eKeyFirst) {
 
 	m_bIsCompleted = IsCompleted(info);
 	m_sCompletionText = GenerateCompletionText(info);
