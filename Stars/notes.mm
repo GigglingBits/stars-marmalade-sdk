f1af045258b585f94dbb968440cfc011d616cb49
Oliver Lanz <oliver.lanz@gigglingbits.com>
2013-12-20 12:50:54 +0100

    Reduced nunber of levels per world from 24 to 18

--- Stars/notes.mm
+++ Stars/notes.mm
@@ -4,16 +4,15 @@
 <node CREATED="1387190150374" ID="ID_57777205" MODIFIED="1387190156714" POSITION="right" TEXT="Gameplay">
 <node CREATED="1387190157205" ID="ID_1530154458" MODIFIED="1387190171047" TEXT="Wait for section to complete before new section is started"/>
 <node CREATED="1387190171491" ID="ID_1168986202" MODIFIED="1387190183030" TEXT="Make world size specific to section"/>
+<node CREATED="1387540124374" ID="ID_1751772566" MODIFIED="1387540142254" TEXT="Make dust collector in a single compartment"/>
 </node>
 <node CREATED="1387190916906" ID="ID_24365676" MODIFIED="1387195654360" POSITION="right" TEXT="Graphical">
 <node CREATED="1387190921114" ID="ID_1792056632" MODIFIED="1387190936069" TEXT="Star shoul look to the right when retracting"/>
 <node CREATED="1387195655244" ID="ID_1170911232" MODIFIED="1387195663381" TEXT="Add glow effect to nuggets"/>
+<node CREATED="1387540152020" ID="ID_1264914265" MODIFIED="1387540162450" TEXT="Star exit must be faster"/>
 </node>
 <node CREATED="1387189726106" ID="ID_813323948" MODIFIED="1387198982909" POSITION="left" TEXT="Errors">
-<node CREATED="1387189732985" ID="ID_65754686" MODIFIED="1387189748523" TEXT="Crash when level map uses a body that does not exits"/>
-<node CREATED="1387198983252" ID="ID_769429521" MODIFIED="1387485707144" TEXT="Progress bar in level 3 is too fast">
-<icon BUILTIN="button_ok"/>
-</node>
+<node CREATED="1387189732985" ID="ID_65754686" MODIFIED="1387540182633" TEXT="Crash when level map uses a body that does not exist"/>
 </node>
 </node>
 </map>
