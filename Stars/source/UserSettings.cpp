58847d5abe9687e237b204bf43b380dd3e37cda2
Oliver Lanz <oliver.lanz@gigglingbits.com>
2014-01-20 21:51:16 +0100

    Extended on user settings

--- Stars/source/UserSettings.cpp
+++ Stars/source/UserSettings.cpp
@@ -1,1 +1,17 @@
 #include "UserSettings.h"
+
+UserSettings::LevelSettings& UserSettings::GetLevels() {
+	return m_xLevels;
+}
+
+UserSettings::LevelSetting& UserSettings::GetLevel(const std::string& name) {
+	return m_xLevels[name];
+}
+
+bool UserSettings::Load(const std::string& filename) {
+	return false;
+}
+
+bool UserSettings::Save(const std::string& filename) {
+	return false;
+}
--- Stars/source/UserSettings.h
+++ Stars/source/UserSettings.h
@@ -1,7 +1,30 @@
 #ifndef __USERSETTINGS_H__ 
 #define __USERSETTINGS_H__ 
 
+#include <string>
+#include <map>
+
 class UserSettings {
+public:
+	struct LevelSetting {
+		int highscore;
+		int stars;
+		LevelSetting() {
+			highscore = 0;
+			stars = 0;
+		}
+	};
+
+private:
+	typedef std::map<std::string, LevelSetting> LevelSettings;
+	LevelSettings m_xLevels;
+
+public:
+	LevelSettings& GetLevels();
+	LevelSetting& GetLevel(const std::string& name);
+	
+	bool Load(const std::string& filename);
+	bool Save(const std::string& filename);
 };
 
 #endif
