4d43267cb756924a136c7ca346c5e4349593f724
Oliver Lanz <oliver.lanz@gigglingbits.com>
2014-05-28 11:58:20 +0200

    Removed soundfx and music options button

--- Stars/source/OptionsPanel.cpp
+++ Stars/source/OptionsPanel.cpp
@@ -3,47 +3,22 @@
 
 OptionsPanel::OptionsPanel(ButtonCommandId cmdid, s3eKey key, long userdata) : 
 ButtonPanel(cmdid, key, userdata),
-m_xSoundFx(eButtonCommandIdSoundFx, s3eKeyFirst),
-m_xMusic(eButtonCommandIdMusic, s3eKeyFirst),
 m_xReloadLocal(eButtonCommandIdReload, s3eKeyAbsGameB, 0),
 m_xReload(eButtonCommandIdReload, s3eKeyAbsGameC, 1) {
-	m_xDemoTimer.ElapsedEvent.AddListener<OptionsPanel>(this, &OptionsPanel::DemoTimerElapsedEventHandler);
 }
 
 OptionsPanel::~OptionsPanel() {
-	m_xDemoTimer.ElapsedEvent.RemoveListener<OptionsPanel>(this, &OptionsPanel::DemoTimerElapsedEventHandler);
 }
 
 void OptionsPanel::Initialize() {
 	m_xReloadLocal.SetTexture(FactoryManager::GetTextureFactory().Create("button_reload_local"));
 	m_xReload.SetTexture(FactoryManager::GetTextureFactory().Create("button_reload"));
-	m_xSoundFx.SetTexture(FactoryManager::GetTextureFactory().Create("button_soundfx"));
-	m_xMusic.SetTexture(FactoryManager::GetTextureFactory().Create("button_music"));
 
 	AddButton(m_xReloadLocal);
 	AddButton(m_xReload);
-	AddButton(m_xSoundFx);
-	AddButton(m_xMusic);
-
-	m_xDemoTimer.Start(1000, 0);
 }
 
 void OptionsPanel::OnUpdate(const FrameData& frame) {
 	ButtonPanel::OnUpdate(frame);
-	m_xDemoTimer.Update(frame.GetRealDurationMs());
 }
 
-void OptionsPanel::DemoTimerElapsedEventHandler(const Timer& sender, const long& data) {
-	// toggle button textures for demo purposes
-	static bool state = false;
-	state = !state;
-	if (state) {
-		m_xSoundFx.SetTexture(FactoryManager::GetTextureFactory().Create("button_soundfx"));
-		m_xMusic.SetTexture(FactoryManager::GetTextureFactory().Create("button_music"));
-	} else {
-		m_xSoundFx.SetTexture(FactoryManager::GetTextureFactory().Create("button_soundfx_mute"));
-		m_xMusic.SetTexture(FactoryManager::GetTextureFactory().Create("button_music_mute"));
-	}
-
-	m_xDemoTimer.Start(1000, 0);
-}
--- Stars/source/OptionsPanel.h
+++ Stars/source/OptionsPanel.h
@@ -6,12 +6,8 @@
 
 class OptionsPanel : public ButtonPanel {
 private:
-	Button m_xSoundFx;
-	Button m_xMusic;
 	Button m_xReload;
 	Button m_xReloadLocal;
-
-	Timer m_xDemoTimer;
 
 public:
 	OptionsPanel(ButtonCommandId cmdid, s3eKey key, long userdata = 0);
@@ -20,9 +16,6 @@ public:
 
 protected:
 	virtual void OnUpdate(const FrameData& frame);
-
-private:
-	void DemoTimerElapsedEventHandler(const Timer& sender, const long& data);
 };
 
 #endif
