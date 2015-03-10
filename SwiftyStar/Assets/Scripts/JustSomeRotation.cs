f2752d93d42757694d37b02bbe97141d3863c8da
Oliver Lanz <oliver.lanz@gigglingbits.com>
2015-03-10 23:11:46 +0100

    Added dummy rotation to add some motion the game screen
    It is officially an app now

---
+++ SwiftyStar/Assets/Scripts/JustSomeRotation.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JustSomeRotation : MonoBehaviour {
+
+	void Start () {
+	}
+	
+	void Update () {
+		// just give it some rotation, so that we can see some movement on the screen
+		transform.Rotate (Time.deltaTime * 20.0f, Time.deltaTime * 20.0f, Time.deltaTime * 20.0f);
+	}
+}
