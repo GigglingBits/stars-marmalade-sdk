3fb6ca802a5f05a507d21ea79d307e12e07cc5cc
Oliver Lanz <oliver.lanz@gigglingbits.com>
2015-03-12 20:49:26 +0100

    Added navigation between scenes

---
+++ SwiftyStar/Assets/Scripts/UIHelper.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIHelper : MonoBehaviour {
+
+	public void OpenGameModeMenuScene() {
+		Application.LoadLevel("GameModeMenu");
+	}
+
+	public void OpenTitleMenuScene() {
+		Application.LoadLevel("TitleMenu");
+	}
+	
+	public void OpenLevelMenuScene() {
+		Application.LoadLevel("LevelMenu");
+	}
+	
+	public void OpenGamePlayScene() {
+		Application.LoadLevel("GamePlay");
+	}
+	
+	public void OpenCompletionMenuScene() {
+		Application.LoadLevel("CompletionMenu");
+	}
+	
+}
