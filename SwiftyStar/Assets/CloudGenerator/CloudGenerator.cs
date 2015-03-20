34247f29e82cb1c1124bf9e89fe2af5232051b5c
Oliver Lanz <oliver.lanz@gigglingbits.com>
2015-03-21 00:32:15 +0100

    Imported all Spine assets

---
+++ SwiftyStar/Assets/CloudGenerator/CloudGenerator.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudGenerator : MonoBehaviour {
+
+	private const float CLOUD_INTERVAL_SEC = 3.0f; 
+	private float countdown;
+
+	public CloudGenerator () {
+		countdown = CLOUD_INTERVAL_SEC;
+	}
+
+	public void Start () {
+
+	}
+	
+	public void Update () {
+		countdown -= Time.deltaTime;
+		if (countdown <= 0.0f) {
+			countdown += CLOUD_INTERVAL_SEC;
+			CreateNextCloud();
+		}
+	}
+	
+	private void CreateNextCloud() {
+		GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube.AddComponent<Rigidbody>();
+		cube.transform.position = new Vector3(0, 0, 0);	
+
+	//	GetComponents
+
+		// create a cloud
+
+	}
+}
