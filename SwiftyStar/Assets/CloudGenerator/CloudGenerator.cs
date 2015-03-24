f98b07ee9aa067e449199937b9dc84ac69ad4b38
Oliver Lanz <oliver.lanz@gigglingbits.com>
2015-03-24 21:55:54 +0100

    New cloud is instantiated every 3 secs

--- SwiftyStar/Assets/CloudGenerator/CloudGenerator.cs
+++ SwiftyStar/Assets/CloudGenerator/CloudGenerator.cs
@@ -1,16 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class CloudGenerator : MonoBehaviour {
 
-	private const float CLOUD_INTERVAL_SEC = 3.0f; 
-	private float _countdown;
+	private const float CLOUD_INTERVAL_SEC = 2.0f; 
+	private int _cloudCounter = 0;
 
-	public CloudGenerator () {
-		_countdown = CLOUD_INTERVAL_SEC;
-	}
+	private float _countdown = CLOUD_INTERVAL_SEC;
+
+	private List<Sprite> _spriteList = new List<Sprite>();
 
 	public void Start () {
+		LoadSprites(_spriteList);
 	}
 	
 	public void Update () {
@@ -20,20 +22,46 @@ public class CloudGenerator : MonoBehaviour {
 			CreateNextCloud();
 		}
 	}
-	
+
+	private void LoadSprites(List<Sprite> list) {
+		list.AddRange(Resources.LoadAll<Sprite> ("cloud01"));
+		list.AddRange(Resources.LoadAll<Sprite> ("cloud02"));
+		list.AddRange(Resources.LoadAll<Sprite> ("cloud03"));
+		list.AddRange(Resources.LoadAll<Sprite> ("cloud04"));
+	}
+
+	private Sprite GetRandomSprite() {
+		return _spriteList[Random.Range(0, _spriteList.Count)];
+	}
+
 	private void CreateNextCloud() {
-		//var cloud = Instantiate<Cloud01>();
+		// instantiate the cloud
+		var prefab = Resources.Load ("CloudPrefab");
+		GameObject cloud = Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity) as GameObject; 
 
-	//	Instantiate<Cloud01> ();
+		// assign the visual appearance
+		string cloudname = "Cloud";
+		var renderer = cloud.GetComponent<SpriteRenderer>();
+		if (renderer != null) {
+			Sprite face = GetRandomSprite ();
+			if (face != null) {
+				cloudname += string.IsNullOrEmpty (face.name) ? "_noname" : ("_" + face.name);
+			} else {
+				cloudname += "_err2";
+			}
+			renderer.sprite = face;
+		} else {
+			cloudname += "_err1";
+		}
+		cloudname += "_" + (++_cloudCounter);
+		cloud.name = cloudname;
 
-/*
- 		GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-		cube.AddComponent<Rigidbody>();
-		cube.transform.position = new Vector3(0, 0, 0);	
-*/
-	//	GetComponents
+		// prevent counter overflow
+		if (_cloudCounter == int.MaxValue) {
+			_cloudCounter = 0;
+		}
 
-		// create a cloud
-
+		// editor cosmetics: oganize it as child of current component
+		cloud.transform.SetParent(this.transform);
 	}
 }
---
+++ SwiftyStar/Assets/CloudGenerator/Resources/CloudLogic.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CloudLogic : MonoBehaviour {
+
+	void Start () {
+		InitializeTransform();
+	}
+	
+	void Update () {
+		Move ();
+		ManageLyfecycle ();
+	}
+
+	void InitializeTransform() {
+		Sprite s = GetComponent<SpriteRenderer> ().sprite;
+
+		transform.localPosition = new Vector3 (8, 0, 0);
+		transform.localScale = new Vector3 (1, 1, 1);
+	}
+	
+	void Move() {
+		var pos = transform.localPosition;
+		pos.x -= 0.03f;
+		transform.localPosition = pos;
+	}
+
+	void ManageLyfecycle() {
+		bool outofbounds = transform.localPosition.x < -8.0f;
+		if (outofbounds) {
+			Destroy(gameObject);
+		}
+	}
+}
