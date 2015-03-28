41d8e73c43ab1781190a0af524036bc8f8543f17
Oliver Lanz <oliver.lanz@gigglingbits.com>
2015-03-28 17:36:37 +0100

    Coupled the canvas to the camera view

---
+++ SwiftyStar/Assets/Game Logic/CameraAdjuster.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraAdjuster : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	
+	
+	
+	// <summary>Returns the size of the world in world units</summary> 
+	private void InitializeCamera ()
+	{
+	}
+
+	// <summary>Returns the size of the world in world units</summary> 
+	private Vector2 GetWorldSize ()
+	{
+		return GameFoundation.instance.worldSize;
+	}
+	
+	// <summary>Returns the size of the world in world units</summary> 
+	private Rect GetView ()
+	{
+		Camera cam = Camera.main;
+		float halfheight = cam.orthographicSize;
+		float halfwidth = halfheight * cam.aspect;
+		
+		Vector3 center = cam.ViewportToWorldPoint (new Vector3 (0, 0, 0));
+		
+		return new Rect (center.x - halfwidth, center.y + halfheight, 2.0f * halfwidth, 2.0f * halfheight);
+	}
+	
+	// <summary>Returns the size of the screen in pixels</summary> 
+	private Vector2 GetScreenSize ()
+	{
+		return new Vector2 (Screen.width, Screen.height);
+	}
+
+
+}
---
+++ SwiftyStar/Assets/Game Logic/GameFoundation.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameFoundation
+{
+	public Vector2 worldSize { get; set; }
+
+	#region singleton
+	// singleton
+	private static GameFoundation _theGame;
+
+	public static GameFoundation instance {
+		get {
+			if (null == _theGame) {
+				_theGame = new GameFoundation ();
+			}
+			return _theGame;
+		}
+	}
+	#endregion singleton
+
+	private GameFoundation ()
+	{
+		worldSize = new Vector2 (12.0f, 8.0f);		
+	}
+
+}
