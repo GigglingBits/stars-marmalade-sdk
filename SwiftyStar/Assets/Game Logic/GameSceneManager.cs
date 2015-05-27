1483c76878eb5b01ce0d0db99622935550344549
Oliver Lanz <oliver.lanz@gigglingbits.com>
2015-05-27 21:54:15 +0200

    Level menu pages and level buttons added

---
+++ SwiftyStar/Assets/Game Logic/GameSceneManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameSceneManager
+{
+	#region singleton
+	private static GameSceneManager _theInstance = null;
+	
+	public static GameSceneManager instance {
+		get {
+			if (null == _theInstance) {
+				_theInstance = new GameSceneManager ();
+			}
+			return _theInstance;
+		}
+	}
+	#endregion
+	
+	public int currentLevel { get; set; }
+
+	public void OpenGameModeMenuScene ()
+	{
+		Application.LoadLevel ("GameModeMenu");
+	}
+	
+	public void OpenTitleMenuScene ()
+	{
+		Application.LoadLevel ("TitleMenu");
+	}
+	
+	public void OpenLevelMenuScene ()
+	{
+		Application.LoadLevel ("LevelMenu");
+	}
+	
+	public void OpenGamePlayScene (int level)
+	{
+		currentLevel = level;
+		Application.LoadLevel ("GamePlay");
+	}
+	
+	public void OpenCompletionMenuScene ()
+	{
+		Application.LoadLevel ("CompletionMenu");
+	}
+}
---
+++ SwiftyStar/Assets/Scenes/LevelMenu.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public interface ILevelMenuButtonInstantiatedHandler : IEventSystemHandler
+{
+	void RegisterButton (int id, ILevelMenuFeedbackHandler button);
+}
+
+public class LevelMenu : MonoBehaviour, ILevelMenuButtonInstantiatedHandler
+{
+	private Dictionary<int, ILevelMenuFeedbackHandler> _buttons = new Dictionary<int, ILevelMenuFeedbackHandler> ();	
+	
+	public int _numberOfLevelPages = 3;
+	public int _currentLevelPage = 0;
+
+	private bool _buttonUpdateNeeded;
+
+	void Start ()
+	{
+		_buttonUpdateNeeded = true;
+	}
+	
+	void Update ()
+	{	
+		if (_buttonUpdateNeeded) {
+			UpdateButtons ();
+		}
+	}
+
+	public void RegisterButton (int id, ILevelMenuFeedbackHandler button)
+	{
+		_buttons.Add (id, button);
+		_buttonUpdateNeeded = true;
+	}	
+
+	private void UpdateButtons ()
+	{
+		foreach (var id in _buttons.Keys) {
+			int level = (_currentLevelPage * _buttons.Count) + id;
+			_buttons [id].SetLevelNumber (level);
+		}
+	}
+
+	public void NextPage ()
+	{
+		_currentLevelPage = (_currentLevelPage + 1) % _numberOfLevelPages;
+		_buttonUpdateNeeded = true;
+	}
+	
+	public void PreviousPage ()
+	{
+		_currentLevelPage = (_numberOfLevelPages + _currentLevelPage - 1) % _numberOfLevelPages;
+		_buttonUpdateNeeded = true;
+	}
+}
